bc681af3974b97cf7867ee3e41ccda65a5587e75
EC2 Default User <ec2-user@ip-172-31-33-2.ec2.internal>
2020-12-09 04:38:22 +0000

    12/8

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//routes
+app.get("/", async function(req, res){
+    res.render("index");
+});
+
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
