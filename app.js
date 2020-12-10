32c4e1937e9f1595eeca7ad6dfc0075d3ca59c6a
EC2 Default User <ec2-user@ip-172-31-33-2.ec2.internal>
2020-12-10 20:40:23 +0000

    12/10

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
+const fakeData = require("faker");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -8,13 +9,21 @@ app.use(express.static("public"));
 //routes
 app.get("/", async function(req, res){
     let keyword = "playstation";
-    let apiUrl = `https://api.unsplash.com/photos/random/?client_id=-3BOO4vv-FqVD_CUht9-NXtA5Pb0NMM0RkPJE6yZjb4&featured=true&orientation=landscape&query=${keyword}`;
+    let apiKey = "-3BOO4vv-FqVD_CUht9-NXtA5Pb0NMM0RkPJE6yZjb4";
+    let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&orientation=landscape&query=${keyword}`;
     let response = await fetch(apiUrl);
     let data = await response.json();
     
-    res.render("index", {"ps4Url": data.urls.small});
+    let randName = fakeData.name.findName();
+    
+    res.render("index", {"ps4Url": data.urls.small, "fakerName":randName});
 });
 
+app.get("/checkout", async function(req, res){
+    let randName = fakeData.name.findName();
+    
+    res.render("checkout", {"fakerName":randName});
+});
 
 //starting server
 app.listen(process.env.PORT, process.env.IP, function(){
--- public/js/functions.js
+++ public/js/functions.js
@@ -1,25 +1,73 @@
 /* global */
 $(document).ready(function(){
     
-    //game1
+    //total of shopping cart
     var total = 0;
+    //game1
     var game1cost = 59.99;
+    $("#game1cost").html(`$${game1cost}`);
     var game1qty = 0;
     var game1Name = "ASSASSINS CREED VALHALLA";
     //game2
-    var total = 0;
     var game2cost = 69.99;
+    $("#game2cost").html(`$${game2cost}`);
     var game2qty = 0;
     var game2Name = "MADDEN 21";
     //game3
-    var total = 0;
     var game3cost = 99.99;
+    $("#game3cost").html(`$${game3cost}`);
     var game3qty = 0;
     var game3Name = "FIFA 21";
+    //game4
+    var game4cost = 59.99;
+    $("#game4cost").html(`$${game4cost}`);
+    var game4qty = 0;
+    var game4Name = "CALL OF DUTY BLACK OPS";
+    //game5
+    var game5cost = 69.99;
+    $("#game5cost").html(`$${game5cost}`);
+    var game5qty = 0;
+    var game5Name = "GHOST";
+    //game6
+    var game6cost = 99.99;
+    $("#game6cost").html(`$${game6cost}`);
+    var game6qty = 0;
+    var game6Name = "NBA 2K21";
+     //game7
+    var game7cost = 59.99;
+    $("#game7cost").html(`$${game7cost}`);
+    var game7qty = 0;
+    var game7Name = "SPIDER MAN";
+    //game8
+    var game8cost = 69.99;
+    $("#game8cost").html(`$${game8cost}`);
+    var game8qty = 0;
+    var game8Name = "JEDI FALLEN ORDER";
+    //game9
+    var game9cost = 99.99;
+    $("#game9cost").html(`$${game9cost}`);
+    var game9qty = 0;
+    var game9Name = "GOD OF WAR";
+    //game10
+    var game10cost = 59.99;
+    $("#game10cost").html(`$${game10cost}`);
+    var game10qty = 0;
+    var game10Name = "CALL OF DUTY BLACK OPS";
+    //game11
+    var game11cost = 69.99;
+    $("#game11cost").html(`$${game11cost}`);
+    var game11qty = 0;
+    var game11Name = "THE INRECEDIBLES";
+    //game12
+    var game12cost = 99.99;
+    $("#game12cost").html(`$${game12cost}`);
+    var game12qty = 0;
+    var game12Name = "SPYRO";
     
     
     $("#game1").on("click", function(){
         game1qty++;
+        sessionStorage.setItem("game1qty_stored", game1qty);
         total += game1cost;
         $("#cart").append(`<tr><td>${game1Name}</td> <td>${1}</td> <td>${game1cost}</td></tr><br>`);
         $("#totalCost").html(`${total}`);
@@ -39,6 +87,70 @@ $(document).ready(function(){
         $("#totalCost").html(`${total}`);
     });
     
+    $("#game4").on("click", function(){
+        game4qty++;
+        sessionStorage.setItem("game4qty_stored", game1qty);
+        total += game4cost;
+        $("#cart").append(`<tr><td>${game4Name}</td> <td>${1}</td> <td>${game4cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
     
+    $("#game5").on("click", function(){
+        game5qty++;
+        total += game5cost;
+        $("#cart").append(`<tr><td>${game5Name}</td> <td>${1}</td> <td>${game5cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game6").on("click", function(){
+        game6qty++;
+        total += game6cost;
+        $("#cart").append(`<tr><td>${game6Name}</td> <td>${1}</td> <td>${game6cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game7").on("click", function(){
+        game7qty++;
+        sessionStorage.setItem("game1qty_stored", game1qty);
+        total += game7cost;
+        $("#cart").append(`<tr><td>${game7Name}</td> <td>${1}</td> <td>${game7cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game8").on("click", function(){
+        game8qty++;
+        total += game8cost;
+        $("#cart").append(`<tr><td>${game8Name}</td> <td>${1}</td> <td>${game8cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game9").on("click", function(){
+        game9qty++;
+        total += game9cost;
+        $("#cart").append(`<tr><td>${game9Name}</td> <td>${1}</td> <td>${game9cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game10").on("click", function(){
+        game10qty++;
+        sessionStorage.setItem("game4qty_stored", game10qty);
+        total += game10cost;
+        $("#cart").append(`<tr><td>${game10Name}</td> <td>${1}</td> <td>${game10cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game11").on("click", function(){
+        game11qty++;
+        total += game11cost;
+        $("#cart").append(`<tr><td>${game11Name}</td> <td>${1}</td> <td>${game11cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
+    
+    $("#game12").on("click", function(){
+        game12qty++;
+        total += game12cost;
+        $("#cart").append(`<tr><td>${game12Name}</td> <td>${1}</td> <td>${game12cost}</td></tr><br>`);
+        $("#totalCost").html(`${total}`);
+    });
     
 });
