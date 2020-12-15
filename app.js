ca8fbc8c7d8abd2c016d2851744afa901f76fac5
seandelacerda <seanpdlc@gmail.com>
2020-12-15 05:39:38 +0000

    update products and cart from db

--- app.js
+++ app.js
@@ -18,70 +18,123 @@ app.use(session({
 var ssn;
 
 //parse POST parameters
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //routes
-app.get("/", async function(req, res){
+app.get("/", async function(req, res) {
     ssn = req.session;
-    
+    ssn.cart = [];
+
     let keyword = "playstation";
     let apiKey = "-3BOO4vv-FqVD_CUht9-NXtA5Pb0NMM0RkPJE6yZjb4";
     let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&featured=true&orientation=landscape&query=${keyword}`;
     let response = await fetch(apiUrl);
-    
-    if (!response.ok){
+
+    if (!response.ok) {
         ssn.pic = "img/ps.jpg";
-    } else{
+    }
+    else {
         let data = await response.json();
         ssn.pic = data.urls.small;
     }
-    
-    res.render("index", {"ps4Url": ssn.pic});
+
+    res.render("index", { "ps4Url": ssn.pic });
 });
 
-app.get("/register", async function(req, res){
+app.get("/register", async function(req, res) {
     ssn.req.session;
-    
-    res.render("register", {"ps4Url": ssn.pic});
+
+    res.render("register", { "ps4Url": ssn.pic });
 });
 
-app.post("/", async function(req, res){
+app.post("/", async function(req, res) {
     ssn = req.session;
     let username = req.body.username;
     let password = req.body.password;
     let randName = fakeData.name.findName();
-    
-    if (username == '' || password == ''){
+    let cart = [];
+    let catalog = [];
+
+    if (username == '' || password == '') {
         req.session.authenticated = false;
-        res.render("index", {"ps4Url": ssn.pic, "loginError":true});
-    } 
+        res.render("index", { "ps4Url": ssn.pic, "loginError": true });
+    }
     let match = await checkCredentials(username, password);
-    if (match.length > 0){
+    if (match.length > 0) {
         req.session.authenticated = true;
-        res.render("product", {"ps4Url": ssn.pic, "fakerName":randName});
-    } else{
+        // store username and id in session
+        req.session.userId = match[0].user_id;
+        req.session.username = match[0].username;
+
+        let products = await getProducts();
+        let items = await getCart(req.session.userId);
+
+        if (products) {
+            catalog = products;
+            req.session.products = products;
+        }
+
+        if (items) {
+            cart = items;
+            req.session.items = items;
+        }
+
+        res.render("product", { "ps4Url": ssn.pic, "fakerName": randName, "products": catalog, "cart": cart });
+    }
+    else {
         req.session.authenticated = false;
-        res.render("index", {"ps4Url": ssn.pic, "loginError":true});
+        res.render("index", { "ps4Url": ssn.pic, "loginError": true });
     }
 });
 
 //routes
-app.get("/product", isAuthenticated, async function(req, res){
-    ssn = req.session;
-    
+app.get("/product", isAuthenticated, async function(req, res) {
+    let cart = [];
+    let catalog = [];
+
+    let products = await getProducts();
+    let items = await getCart(req.session.userId);
+
+    if (products) {
+        catalog = products;
+        req.session.products = products;
+    }
+
+    if (items) {
+        cart = items;
+        req.session.items = items;
+    }
+
     let randName = fakeData.name.findName();
-    
-    res.render("product", {"ps4Url": ssn.pic, "fakerName":randName});
+
+    res.render("product", { "ps4Url": req.session.pic, "fakerName": randName, "products": catalog, "cart": cart });
 });
 
-app.get("/checkout", async function(req, res){
-    ssn = req.session;
+app.get("/checkout", async function(req, res) {
+    let items = await getCart(req.session.userId);
+    
+    let cart = items ? items : req.session.items;
     
     let randName = fakeData.name.findName();
-    res.render("checkout", {"ps4Url": ssn.pic, "fakerName":randName});
+    res.render("checkout", { "ps4Url": req.session.pic, "fakerName": randName, "cart": cart });
 });
 
-function createDBConnection(){
+app.post("/addCart", async function(req, res) {
+    let productId = parseInt(req.body.productId);
+    let productName = req.body.productName;
+    let productCost = req.body.productCost;
+    console.log(req.body)
+    let randName = fakeData.name.findName();
+
+    let product = await addToCart(productId, productName, productCost, 1, req.session.userId);
+    let items = await getCart(req.session.userId);
+    
+    let cart = items ? items : req.session.items;
+
+    res.render("product", { "ps4Url": req.session.pic, "fakerName": randName, "products": req.session.products, "cart": cart });
+});
+
+function createDBConnection() {
     var conn = mysql.createPool({
         connectionLimit: 10, // heroku JawsDB connection limit
         host: 'r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -92,27 +145,64 @@ function createDBConnection(){
     return conn;
 }
 
-function isAuthenticated(req, res, next){
-    if (!req.session.authenticated){
+function isAuthenticated(req, res, next) {
+    if (!req.session.authenticated) {
         res.redirect('/');
-    } else{
+    }
+    else {
         next();
     }
 }
 
-function checkCredentials(username, password){
+function checkCredentials(username, password) {
     let sql = "SELECT * FROM customer WHERE username = ? AND password = ?";
-    return new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject) {
         let conn = createDBConnection();
-        conn.query(sql, [username, password], function (err, rows, fields){
+        conn.query(sql, [username, password], function(err, rows, fields) {
             if (err) throw err;
             resolve(rows);
-        });//query
-    });//promise
-    
+        }); //query
+    }); //promise
+
+}
+
+function addToCart(productId, name, cost, quantity, userId) {
+    let sql = "INSERT INTO cartItems (id, productId, productName, productCost, quantity, userId) VALUES (?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE quantity = quantity + 1";
+    let params = ['DEFAULT', productId, name, cost, quantity, userId];
+    console.log("attempting to add to cart (params): ", params);
+    return new Promise(function(resolve, reject) {
+        let conn = createDBConnection();
+        conn.query(sql, params, function(err, rows, fields) {
+            if (err) throw err;
+            resolve(rows);
+        }); //query
+    }); //promise
+}
+
+function getCart(userId) {
+    let sql = "SELECT * FROM cartItems WHERE userId = ?";
+    let params = [userId];
+    return new Promise(function(resolve, reject) {
+        let conn = createDBConnection();
+        conn.query(sql, params, function(err, rows, fields) {
+            if (err) throw err;
+            resolve(rows);
+        }); //query
+    }); //promise
+}
+
+function getProducts() {
+    let sql = "SELECT * FROM product";
+    return new Promise(function(resolve, reject) {
+        let conn = createDBConnection();
+        conn.query(sql, function(err, rows, fields) {
+            if (err) throw err;
+            resolve(rows);
+        }); //query
+    }); //promise
 }
 
 //starting server
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Express server is running...");
 });
